06cf8eb7f37088e804c085f903a1738e9c95d7aa
Rubén Caro <ruben.caro.estevez@gmail.com>
2017-01-08 17:27:18 +0100

    Add credo&cover

--- mix.exs
+++ mix.exs
@@ -6,13 +6,24 @@ defmodule Populator.Mixfile do
      version: "0.5.0",
      elixir: ">= 1.0.0",
      package: package(),
-     deps: [{:ex_doc, ">= 0.0.0", only: :dev}],
-     description: "Supervisor population control library"]
+     deps: deps(),
+     description: "Supervisor population control library",
+     test_coverage: [tool: Populator.Helpers.Cover, verbose: false, ignored: []],
+     aliases: aliases()]
   end
 
   defp package do
     [maintainers: ["Rubén Caro"],
      licenses: ["MIT"],
      links: %{github: "https://github.com/rubencaro/populator"}]
+  end
+
+  defp deps do
+    [{:ex_doc, ">= 0.0.0", only: :dev},
+     {:credo, "~> 0.4", only: [:dev, :test]}]
+  end
+
+  defp aliases do
+    [test: ["test --cover", "credo"]]
   end
 end
